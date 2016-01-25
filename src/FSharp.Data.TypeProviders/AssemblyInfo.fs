b4da87b68c5c8b0c512d39bd96723ea788dd0d7f
Don Syme <donsyme@fastmail.fm>
2016-01-25 18:30:50 +0000

    version update

--- src/FSharp.Data.TypeProviders/AssemblyInfo.fs
+++ src/FSharp.Data.TypeProviders/AssemblyInfo.fs
@@ -6,7 +6,6 @@ open System.Reflection
 [<assembly: AssemblyDescriptionAttribute("F# Type Providers using .NET Framework generators, was FSharp.Data.TypeProviders.dll")>]
 [<assembly: AssemblyVersionAttribute("5.0.0.0")>]
 [<assembly: AssemblyFileVersionAttribute("5.0.0.0")>]
-[<assembly:System.Runtime.CompilerServices.InternalsVisibleTo("FSharp.Data.TypeProviders.Tests")>]
 do ()
 
 module internal AssemblyVersionInformation =
