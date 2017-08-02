a77d7ca57cabf5939a66d2bd1f1b86afbfb49d94
Marcin Malinowski <orientman@gmail.com>
2017-08-02 11:08:07 +0200

    Support for additional (4.6.x) SDK registry keys

--- src/FSharp.Data.TypeProviders/Util.fs
+++ src/FSharp.Data.TypeProviders/Util.fs
@@ -104,6 +104,8 @@ module internal Util =
                     reg32view.Dispose()     // if reg32view were really null, we would not be here and the user would have more serious issues not being able to access HKLM
 
         let SDK_REGPATHS = [ @"Software\Microsoft\Microsoft SDKs\NETFXSDK\4.6\WinSDK-NetFx40Tools"
+                             @"Software\Microsoft\Microsoft SDKs\NETFXSDK\4.6.1\WinSDK-NetFx40Tools"
+                             @"Software\Microsoft\Microsoft SDKs\NETFXSDK\4.6.2\WinSDK-NetFx40Tools"
                              @"Software\Microsoft\Microsoft SDKs\Windows\v8.1A\WinSDK-NetFx40Tools"
                              @"Software\Microsoft\Microsoft SDKs\Windows\v8.0A\WinSDK-NetFx40Tools"
                              @"Software\Microsoft\Microsoft SDKs\Windows\v7.1\WinSDK-NetFx40Tools"
